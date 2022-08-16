134d2e20674722924b8e1f8d98d601ab8a0ac702
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-08-16 15:04:33 +0530

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -12,12 +12,6 @@ Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 void setup()
 {
   Serial.begin(9600);
-
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // Address 0x3D for 128x64
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;); // Don't proceed, loop forever
-  }
 }
 
 void loop(){
