b9e81239f041f72a009510a64f9f2f7da40cc1a2
Ritika999 <70030520+Ritika999@users.noreply.github.com>
2023-10-01 22:11:17 +0530

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -9,9 +9,27 @@
 // Declaration for SSD1306 display connected using software SPI (default case):
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+//  button objects
+ezButton button1(13);
+ezButton button2(33);
+ezButton button3(14);
+ezButton button4(27);
+ezButton button5(26);
+
+int vote1 = 0, vote2 = 0, vote3 = 0, vote4 = 0;
+
+int flag = 0;
+
 void setup()
 {
   Serial.begin(9600);
+  
+   if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  };
+
+  
 }
 
 void loop(){
